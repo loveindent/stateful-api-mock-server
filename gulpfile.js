0cdfa4080067dd7d64ec224ac0c127c7a6e7ff02
Hikkyu (Damien Berseron) <damien.berseron@me.com>
2016-08-08 11:46:12 +0200

    try npm travis publish

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var plumber = require('gulp-plumber');
 var runSequence = require('run-sequence');
 var install = require('gulp-install');
 var shrinkwrap = require('gulp-shrinkwrap');
+var gzip = require('gulp-gzip');
+var tar = require('gulp-tar');
 
 gulp.task('clean', function(cb) {
   return del(['./dist'], cb);
@@ -35,12 +37,21 @@ gulp.task('shrinkwrap', function() {
   ;
 });
 
+gulp.task('zip', function() {
+  return gulp.src('dist/**/*')
+    .pipe(tar('archive.tar'))
+    .pipe(gzip())
+    .pipe(gulp.dest('.'))
+  ;
+});
+
 gulp.task('build', function(callback) {
   runSequence(
     'clean',
     'copy',
     'shrinkwrap',
     'npm-install',
+    'zip',
     callback
   );
 });
